62e7ea2b0e0a6bfa87e1dc9a2908057e75fdf6c2
Gaba <gabrielsps04@gmail.com>
2023-09-14 19:01:43 -0300

    Cria funcao de selecionar e armazenar a Data e Hora para a enviar a notificacao

--- app/src/main/java/com/project/lembretio/EventActivity.kt
+++ app/src/main/java/com/project/lembretio/EventActivity.kt
@@ -1,23 +1,43 @@
 package com.project.lembretio
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import java.util.Calendar
 
 
-class EventActivity : AppCompatActivity() {
+class EventActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     private lateinit var mainText: TextView
     private lateinit var editText: EditText
     private lateinit var submitButton: Button
     private lateinit var cancelButton: Button
+    private lateinit var setDateButton: Button
+
+    var day = 0
+    var month = 0
+    var year = 0
+    var hour = 0
+    var minute = 0
+
+    var savedDay = 0
+    var savedMonth = 0
+    var savedYear = 0
+    var savedHour = 0
+    var savedMinute = 0
+
+
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +88,39 @@ class EventActivity : AppCompatActivity() {
             startActivity(Intent(applicationContext, MainActivity::class.java))
         }
 
+        pickDate()
 
+    }
+
+    private fun getTimeCalendar() {
+        val cal : Calendar = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+    }
+
+    private fun pickDate() {
+        setDateButton = findViewById(R.id.btnSaveDate)
+        setDateButton.setOnClickListener {
+            DatePickerDialog(this, this, year, month, day).show()
+        }
+
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay = dayOfMonth
+        savedMonth = month
+        savedYear = year
+
+        getTimeCalendar()
+
+        TimePickerDialog(this,this,hour,minute,true).show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
     }
 }
