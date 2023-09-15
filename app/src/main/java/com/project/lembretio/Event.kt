c45bd5481c96aa5d024d3850a89797cac055db6c
Elias Santos Martins <eliasgjt@gmail.com>
2023-09-15 17:44:50 -0300

    corrige nome database

--- app/src/main/java/com/project/lembretio/Event.kt
+++ app/src/main/java/com/project/lembretio/Event.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "event_table")
 data class Event(
 
     @ColumnInfo(name = "name") var name: String,
