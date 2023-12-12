c28abf593375fa86a1143770e16b99d668dd7491
Gabriel Sanders <gabrielsps04@gmail.com>
2023-12-12 13:22:55 -0300

    refactoring em timelineactivity (commit de Elias)

--- app/src/main/java/com/project/lembretio/TimelineActivity.kt
+++ app/src/main/java/com/project/lembretio/TimelineActivity.kt
@@ -3,8 +3,10 @@ package com.project.lembretio
 
 
 import android.app.AlarmManager
+import android.app.AlertDialog
 import android.app.PendingIntent
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -21,33 +23,49 @@ class TimelineActivity : AppCompatActivity() {
     private fun countDoses(event: Event): Int {
         return 0
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timeline)
 
-        // Receba o evento da Intent
-        val receivedEvent: Event? = intent.getParcelableExtra("event")
+    private fun deleteEvent(event: Event) {
+
+        val alarmIntent = Intent(applicationContext, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            event.alarmId,
+            alarmIntent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        pendingIntent?.let { _pendingIntent ->
+            alarmManager.cancel(_pendingIntent)
+        }
+
+        eventViewModel.deleteEvent(event)
 
 
-        val titleText = findViewById<TextView>(R.id.remedio)
-        val dateinit = findViewById<TextView>(R.id.inicio)
-        val timing = findViewById<TextView>(R.id.timing)
-        val progresso = findViewById<TextView>(R.id.progresso)
-        val alarme = findViewById<TextView>(R.id.alarme)
+        Toast.makeText(applicationContext, "Evento ${event.name} excluído", Toast.LENGTH_SHORT).show()
+        val intentBack = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intentBack)
+    }
+    private fun showDeleteConfirmationDialog(event: Event) {
+        val builder = AlertDialog.Builder(this)
 
+        builder.setTitle("Excluir")
+        builder.setMessage("Tem certeza que quer excluir ${event.name}?")
+
+        builder.setPositiveButton("Sim") { dialogInterface: DialogInterface, i: Int ->
+            deleteEvent(event)
+        }
+
+        builder.setNegativeButton("Não") { dialogInterface: DialogInterface, i: Int ->
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun configureButtons(receivedEvent: Event) {
         val btn_edit = findViewById<Button>(R.id.btn_time_edit)
         val btn_del = findViewById<Button>(R.id.btn_time_del)
         val btn_prev = findViewById<Button>(R.id.btn_time_prev)
-
-        if (receivedEvent != null) {
-            titleText.text = receivedEvent.name
-            dateinit.text = "Data de início: ${receivedEvent.createdDateFormatted}"
-
-            if (receivedEvent.repeating) {
-                timing.text = "Horários do dia: ${receivedEvent.times.joinToString(separator = ", ") { it.toString() }}"
-                progresso.text = "Doses tomadas: ${countDoses(receivedEvent)}"
-            }
-        }
 
         btn_edit.setOnClickListener {
             val intentEdit = Intent(applicationContext, EventPagerActivity::class.java)
@@ -55,34 +73,41 @@ class TimelineActivity : AppCompatActivity() {
             startActivity(intentEdit)
         }
 
-        if (receivedEvent != null) {
-            btn_del.setOnClickListener {
-                val alarmIntent = Intent(applicationContext, AlarmReceiver::class.java)
-                val pendingIntent = PendingIntent.getBroadcast(
-                    applicationContext,
-                    receivedEvent.alarmId,
-                    alarmIntent,
-                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-                )
-                val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                pendingIntent?.let { _pendingIntent ->
-                    alarmManager.cancel(_pendingIntent)
-                }
-
-                eventViewModel.deleteEvent(receivedEvent)
-
-
-                Toast.makeText(applicationContext, "Evento ${receivedEvent.name} excluído", Toast.LENGTH_SHORT).show()
-                val intentBack = Intent(applicationContext, MainActivity::class.java)
-                startActivity(intentBack)
-            }
+        btn_del.setOnClickListener {
+            showDeleteConfirmationDialog(receivedEvent)
         }
 
         btn_prev.setOnClickListener {
             val intentBack = Intent(applicationContext, MainActivity::class.java)
             startActivity(intentBack)
         }
+    }
 
+    private fun configureTexts(receivedEvent: Event){
+        val titleText = findViewById<TextView>(R.id.remedio)
+        val dateinit = findViewById<TextView>(R.id.inicio)
+        val timing = findViewById<TextView>(R.id.timing)
+        val progresso = findViewById<TextView>(R.id.progresso)
+        val alarme = findViewById<TextView>(R.id.alarme)
 
+        titleText.text = receivedEvent.name
+        dateinit.text = "Data de início: ${receivedEvent.createdDateFormatted}"
+
+        if (receivedEvent.repeating) {
+            timing.text = "Horários do dia: ${receivedEvent.times.joinToString(separator = ", ") { it.toString() }}"
+            progresso.text = "Doses tomadas: ${countDoses(receivedEvent)}"
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline)
+
+        val receivedEvent: Event? = intent.getParcelableExtra("event")
+
+        if (receivedEvent != null) {
+            configureTexts(receivedEvent)
+            configureButtons(receivedEvent)
+        }
     }
 }
