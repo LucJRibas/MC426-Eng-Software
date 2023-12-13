307669bba9cb9ee607956ab4c2fec7580ad6f56c
Gabriel Sanders <gabrielsps04@gmail.com>
2023-12-13 15:50:27 -0300

    testes database 4/5 (commit de Elias)

--- app/src/androidTest/java/com/project/lembretio/EventDaoTest.kt
+++ app/src/androidTest/java/com/project/lembretio/EventDaoTest.kt
@@ -1,13 +1,15 @@
-package com.project.lembretio
-
 import android.content.Context
+import android.net.Uri
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.project.lembretio.AppDatabase
+import com.project.lembretio.Event
+import com.project.lembretio.EventDao
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNull
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -16,6 +18,8 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
+import java.time.LocalDate
+import java.time.LocalTime
 
 @RunWith(AndroidJUnit4::class)
 class EventDaoTest {
@@ -32,7 +36,6 @@ class EventDaoTest {
         eventDao = db.eventDao()
     }
 
-
     @Throws(IOException::class)
     @After
     fun closeDb() {
@@ -42,23 +45,49 @@ class EventDaoTest {
     @Test
     @Throws(Exception::class)
     fun insertAndRetrieveEvent() {
-        val event = Event(name = "Test Event", repeating = false)
+        val event = Event(
+            name = "Test Event",
+            repeating = false,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 1,
+            uri = null,
+            isMedication = true,
+            comment = "Test comment"
+        )
 
         runBlocking {
             eventDao.insert(event)
             val id = eventDao.getAllEvents().first().first().id
             val retrievedEvent = eventDao.getEventById(id)
-            assert(retrievedEvent != null)
-            assert(retrievedEvent?.name == event.name)
-            assert(retrievedEvent?.repeating == event.repeating)
+            assertNotNull(retrievedEvent)
+            assertEquals(event.name, retrievedEvent?.name)
+            assertEquals(event.repeating, retrievedEvent?.repeating)
+            assertEquals(event.date, retrievedEvent?.date)
+            assertEquals(event.time, retrievedEvent?.time)
+            assertEquals(event.times, retrievedEvent?.times)
+            assertEquals(event.alarmId, retrievedEvent?.alarmId)
+            assertEquals(event.uri, retrievedEvent?.uri)
+            assertEquals(event.isMedication, retrievedEvent?.isMedication)
+            assertEquals(event.comment, retrievedEvent?.comment)
         }
-
     }
 
     @Test
     @Throws(Exception::class)
     fun updateEvent() = runBlocking {
-        val event = Event(name = "Test Event", repeating = false)
+        val event = Event(
+            name = "Test Event",
+            repeating = false,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 1,
+            uri = null,
+            isMedication = true,
+            comment = "Test comment"
+        )
         eventDao.insert(event)
 
         val id = eventDao.getAllEvents().first().first().id
@@ -68,36 +97,61 @@ class EventDaoTest {
         eventDao.update(updatedEvent)
 
         val retrievedEvent = eventDao.getEventById(id)
-        assert(retrievedEvent != null) { "Event should not be null" }
-        assert(retrievedEvent?.name == updatedEvent.name) { "Event should have been updated (name: ${retrievedEvent?.name} expected: ${updatedEvent.name}"}
+        assertNotNull(retrievedEvent)
+        assertEquals(updatedEvent.name, retrievedEvent?.name)
     }
-
 
     @Test
     @Throws(Exception::class)
     fun deleteEvent() = runBlocking {
-
-        val event = Event(name = "Test Event", repeating = false)
+        val event = Event(
+            name = "Test Event",
+            repeating = false,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 1,
+            uri = null,
+            isMedication = true,
+            comment = "Test comment"
+        )
         eventDao.insert(event)
         val insertedEventId = event.id
 
         eventDao.delete(event.copy(id = insertedEventId))
 
         val retrievedEvent = eventDao.getEventById(insertedEventId)
-        assert(retrievedEvent == null) { "Event should have been deleted" }
+        assertNull(retrievedEvent)
     }
-
-
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun testGetAllEvents() {
-        val event1 = Event(name = "Event 1", repeating = false)
-        val event2 = Event(name = "Event 2", repeating = true)
+        val event1 = Event(
+            name = "Event 1",
+            repeating = false,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 1,
+            uri = null,
+            isMedication = true,
+            comment = "Test comment"
+        )
+        val event2 = Event(
+            name = "Event 2",
+            repeating = true,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 2,
+            uri = null,
+            isMedication = false,
+            comment = ""
+        )
 
         val allEvents = mutableListOf<Event>()
 
-        // Inserir eventos no banco de dados
         runBlocking {
             val testDispatcher = StandardTestDispatcher()
 
@@ -107,28 +161,56 @@ class EventDaoTest {
             allEvents.addAll(eventDao.getAllEvents().first())
         }
 
-        assertEquals(2, allEvents.size) // Verifica se há dois eventos
+        assertEquals(2, allEvents.size)
         assertEquals(event1.name, allEvents[0].name)
         assertEquals(event2.name, allEvents[1].name)
     }
 
     @Test
     fun testGetEventById() {
-        val event1 = Event(name = "Event 1", repeating = false)
-        val event2 = Event(name = "Event 2", repeating = true)
-        val event3 = Event(name = "Event 3", repeating = false)
+        val event1 = Event(
+            name = "Event 1",
+            repeating = false,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 1,
+            uri = null,
+            isMedication = true,
+            comment = "Test comment"
+        )
+        val event2 = Event(
+            name = "Event 2",
+            repeating = true,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 2,
+            uri = null,
+            isMedication = false,
+            comment = ""
+        )
+        val event3 = Event(
+            name = "Event 3",
+            repeating = false,
+            date = LocalDate.now(),
+            time = LocalTime.now(),
+            times = mutableListOf(LocalTime.now().plusMinutes(1)),
+            alarmId = 3,
+            uri = null,
+            isMedication = true,
+            comment = "Another comment"
+        )
 
-        // Inserir os eventos no banco de dados
         runBlocking {
             eventDao.insert(event1)
             eventDao.insert(event2)
             eventDao.insert(event3)
         }
 
-        // Obter os eventos pelo ID
-        val retrievedEvent1 = eventDao.getEventById(1) // Supondo que o primeiro evento tenha um ID de 1
-        val retrievedEvent2 = eventDao.getEventById(2)
-        val retrievedEvent3 = eventDao.getEventById(3)
+        val retrievedEvent1 = eventDao.getEventById(event1.id)
+        val retrievedEvent2 = eventDao.getEventById(event2.id)
+        val retrievedEvent3 = eventDao.getEventById(event3.id)
 
         assertNotNull(retrievedEvent1)
         assertNotNull(retrievedEvent2)
