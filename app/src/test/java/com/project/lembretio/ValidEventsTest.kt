7f857cc2d7947f23150545054209a62d2f1b89f9
luc16 <luc.joffily.ribas@gmail.com>
2023-12-15 21:31:37 -0300

    Casos teste para evento inválido

---
+++ app/src/test/java/com/project/lembretio/ValidEventsTest.kt
@@ -0,0 +1,56 @@
+package com.project.lembretio
+
+import com.project.lembretio.utils.isEventTimeValid
+import junit.framework.TestCase.assertEquals
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+class ValidEventsTest {
+
+    private val event: Event = Event(
+        name = "Medication Event",
+        repeating = true,
+        date = LocalDate.now(),
+        time = LocalTime.now(),
+        times = mutableListOf(),
+        alarmId = 1,
+        uri = null,
+        isMedication = true,
+        comment = "Comentario"
+    )
+
+    @Test
+    fun `event is medication and repeating`() {
+        event.isMedication = true
+        event.repeating = true
+        assert(isEventTimeValid(event))
+    }
+
+    @Test
+    fun `invalid event that is medication but not repeating`() {
+        event.isMedication = true
+        event.repeating = false
+        event.date = LocalDate.now()
+        val now = LocalTime.now()
+        event.times = mutableListOf(now.minusMinutes(2), now.plusMinutes(10))
+
+        assert(!isEventTimeValid(event))
+    }
+
+    @Test
+    fun `invalid event that is not medication`() {
+        event.isMedication = false
+        event.repeating = false
+        event.date = LocalDate.now()
+        val now = LocalTime.now()
+        event.times = mutableListOf(now.minusMinutes(1))
+
+        assert(!isEventTimeValid(event))
+    }
+
+}
